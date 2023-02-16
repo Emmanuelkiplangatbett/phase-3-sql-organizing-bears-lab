2bbeaeca95c0e048d5912f0f137d03efa739fc29
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2023-02-16 17:33:43 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(" Mr.Chocolate", 7, "M", "chocolate", "mild", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Rowdy', 6, 'M', 'black', 'rowdy', true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Tabitha', NULL, 'F', 'brown', NULL, false);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Seargent Brown', 10, 'M', 'brown', 'angry', true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Melissa', 7, 'F', 'black', 'mild', true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Grinch', 11, 'M', 'green', 'angry', false);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Wendy', 3, 'F', 'white', 'rowdy', true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, NULL, 'M', 'white', NULL, true);
